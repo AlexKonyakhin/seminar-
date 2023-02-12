d3d901a4652f4babb3805896392c61986cf09620
Mr. Alex <alex_konyakhin@mail.ru>
2023-02-12 11:54:46 +0300

    задание 4

---
+++ Homeworks/Homework1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a > b)
+// {
+//     Console.WriteLine("Maximum is" + " " + a);
+
+// }
+// else
+// {
+//     Console.WriteLine("Maximum is" + " " + b);
+// }
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+
+// if(b > max)
+// {
+//     max = b;
+// }
+
+// if(c > max)
+// {
+//    max = c; 
+// }
+
+// Console.WriteLine("Maximum is" + " " + max);
